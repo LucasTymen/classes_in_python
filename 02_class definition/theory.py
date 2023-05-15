708de9e84bbf78c15d22002f6197c051f1b0f7d6
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-15 10:33:45 +0200

    02_class definition

---
+++ 02_class definition/theory.py
@@ -0,0 +1,33 @@
+"""
+
+Introduction to Classes
+Class
+
+A class is a template for a data type. It describes the kinds of information that class will hold and how a programmer
+will interact with that data. Define a class using the class keyword. PEP 8 Style Guide for Python Code recommends
+capitalizing the names of classes to make them easier to identify.
+
+class CoolClass:
+  pass
+
+In the above example we created a class and named it CoolClass. We used the pass keyword in Python to indicate that the
+body of the class was intentionally left blank so we don’t cause an IndentationError. We’ll learn about all the things
+we can put in the body of a class in the next few exercises.
+Instructions
+1.
+
+Define an empty class called Facade. We’ll chip away at it soon!
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+Community Forums
+
+Here are some helpful links to the top questions asked by coders about this exercise:
+
+    What does the pass keyword do?
+
+What is a namespace in python?
+What is the difference between a class and type?
+
+Still have questions? View this exercise's thread in the Codecademy Forums.
+
+"""
